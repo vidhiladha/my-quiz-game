6e5356ba87434068e31ebc5a5f90d2f8ff258093
vidhiladha <74768975+vidhiladha@users.noreply.github.com>
2021-02-20 21:00:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var canva;
+var distance = 0;
+var gameState = 0;
+var contestantCount;
+var database;
+var quiz;
+var question;
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+
+  
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
